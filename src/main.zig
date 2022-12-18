725372fd3d1a3c6ce2314355c24a32499c91f11c
Shojin Masuda <shomasd@gmail.com>
2022-12-18 10:49:56 +0900

    misterious bug!

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const Tag = enum { list, int };
 
 const Expr =
     union(Tag) {
-    list: []Expr,
+    list: []const Expr,
     int: i32,
 };
 
@@ -20,6 +20,10 @@ fn initState(str: []const u8) *ParseState {
 
 fn hasNextChar(str: []const u8, state: *ParseState) bool {
     return str.len - 1 > state.currentIndex;
+}
+
+fn isNotEnded(str: []const u8, state: *ParseState) bool {
+    return str.len > state.currentIndex;
 }
 
 fn nextChar(str: []const u8, state: *ParseState) void {
@@ -37,12 +41,17 @@ fn isInt(state: *ParseState) bool {
     return '0' <= state.currentChar and state.currentChar <= '9';
 }
 
-fn parseInt(str: []const u8, state: *ParseState) anyerror!i32 {
+fn parseInt(str: []const u8, state: *ParseState) i32 {
     var res: i32 = 0;
-    while (hasNextChar(str, state) and '0' <= state.currentChar and state.currentChar <= '9') {
+
+    while (isNotEnded(str, state) and isInt(state)) {
         res = res * 10 + (state.currentChar - '0');
+        if (!hasNextChar(str, state)) {
+            break;
+        }
         nextChar(str, state);
     }
+    std.log.debug("parseint end {}", .{state.currentIndex});
     return res;
 }
 
@@ -55,42 +64,54 @@ const Buffer = struct {
 fn parseExpression(str: []const u8, state: *ParseState) anyerror!Expr {
     skipWS(str, state);
     if (isInt(state)) {
-        const res = try parseInt(str, state);
-        //std.log.debug("{}", .{res});
+        const res = parseInt(str, state);
         return Expr{ .int = res };
     } else {
         var buf: Buffer = .{ .items = try allocator.alloc(Expr, 10), .size = 0, .capacity = 10 };
-        while (hasNextChar(str, state) and state.currentChar != ')') {
+        defer allocator.free(buf.items);
+
+        debug("bufsize {}", .{buf.size});
+        while (isNotEnded(str, state) and state.currentChar != ')') {
             const item = try parseExpression(str, state);
             skipWS(str, state);
 
+            debug("bufsize {}", .{buf.size});
+            // bufにparseStateが入っている？？？
             if (buf.size >= buf.capacity) {
                 const newCapacity = buf.capacity * 2;
                 buf = .{ .items = try allocator.realloc(buf.items, newCapacity), .size = buf.size, .capacity = newCapacity };
             }
 
+            debug("bufsize {}", .{buf.size});
             buf.items[buf.size] = item;
             buf.size += 1;
+            if (!hasNextChar(str, state)) {
+                break;
+            }
             nextChar(str, state);
         }
 
-        const res = Expr{ .list = try allocator.dupe(Expr, buf.items) };
-
-        allocator.free(buf.items);
-        return res;
+        std.log.debug("parseExpr end {}", .{state.currentIndex});
+        return Expr{ .list = try allocator.dupe(Expr, buf.items) };
     }
 }
+
+const debug = std.log.debug;
 
 fn parse(str: []const u8) anyerror!Expr {
     return try parseExpression(str, initState(str));
 }
 
 pub fn main() anyerror!void {
-    const input: []const u8 = "123";
+    // const input = std.os.argv[1];
+    const input = "(123)";
     std.log.info("{}", .{try parse(input)});
-    std.log.info("All your codebase are belong to us.", .{});
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "int test" {
+    try std.testing.expectEqual(Expr{ .int = 123 }, try parse("123"));
+}
+
+test "expr test" {
+    try std.testing.expectEqual(Expr{ .list = &.{Expr{ .int = 123 }} }, try parse("(123)"));
 }
